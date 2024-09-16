0e3d7f6c7b10d1dd241533acb9690364281a6881
Josh Nichols <josh.nichols@gusto.com>
2024-09-16 10:28:00 -0400

    group things

--- lua/macos/init.lua
+++ lua/macos/init.lua
@@ -12,45 +12,50 @@ function M.setup(opts)
 
   opts = vim.tbl_deep_extend('force', defaults, opts)
 
-  -- Copy
+  -- ⌘ c - Copy
+  -- Visual
   vim.keymap.set('v', '<D-c>', '"+y', silent_noremap)
 
-  -- Cut
+  -- ⌘ x - Cut
+  -- Visual Cut
   vim.keymap.set('v', '<D-x>', '"+c', silent_noremap)
 
-  -- Paste
-  vim.keymap.set('', '<D-v>', '"+p', silent_noremap)
+  -- ⌘ v - Paste
+  vim.keymap.set('n', '<D-v>', '"+p', silent_noremap)
+  -- Command
   vim.keymap.set('c', '<D-v>', '<C-r>+', silent_noremap)
+  -- Insert
   vim.keymap.set('i', '<D-v>', '<C-r>+', silent_noremap)
+  -- Terminal
   vim.keymap.set('t', '<D-v>', '<C-\\><C-n>"+pi', silent_noremap)
 
-  -- ⌘ s - save
+  -- ⌘ s - Save
   vim.keymap.set('n', '<D-s>', ':w<CR>', silent)
   vim.keymap.set('n', '<leader>s', ':w<CR>', silent)
   vim.api.nvim_set_keymap('n', '<C-S>', ':%s/', silent_noremap)
 
-  -- ⌘ a - select all
+  -- ⌘ a - Select All
   vim.keymap.set('n', '<D-a>', 'ggVG', silent)
 
-  -- ⌘ / - close current window
-  vim.keymap.set('', '<D-w>', '<cmd>' .. opts.close_command .. '<cr>', silent)
+  -- ⌘ w - close current window
+  vim.keymap.set('n', '<D-w>', '<cmd>' .. opts.close_command .. '<cr>', silent)
+  -- Insert
   vim.keymap.set('i', '<D-w>', '<cmd>' .. opts.close_command .. '<cr>', silent)
 
-  -- Create new tab with Command-T
+  -- ⌘ a - New Tab
   vim.keymap.set('', '<D-t>', '<cmd>tabnew<cr>', silent_noremap)
   vim.keymap.set('i', '<D-t>', '<cmd>tabnew<cr>', silent_noremap)
 
-  -- IDE-like behvaior --
-  -- ⌘ / - toggle comment
+  -- ⌘ / - Toggle comment
   vim.keymap.set('n', '<D-/>', 'gcc', silent)
   vim.keymap.set('v', '<D-/>', 'gc', silent)
 
-  -- ⌘ ]  - shift identation right
+  -- ⌘ ]  - Shift identation right
   vim.keymap.set('n', '<D-]>', '>>', silent)
   vim.keymap.set('v', '<D-]>', '>', silent)
   vim.keymap.set('i', '<D-]>', '<C-O>>>', silent)
 
-  -- ⌘ ] - shift identation left vim.keymap.set("n", "<D-[>", "<<", silent)
+  -- ⌘ ] - Shift identation left vim.keymap.set("n", "<D-[>", "<<", silent)
   vim.keymap.set('n', '<D-[>', '<<', silent)
   vim.keymap.set('v', '<D-[>', '<', silent)
   vim.keymap.set('i', '<D-[>', '<C-O><<', silent)
